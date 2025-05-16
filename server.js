b1c499b98fce43d61665a5802abfb22806f304f7
Roman Lakhnov <lakhnov.roman@gmail.com>
2025-05-17 01:08:07 +0300

    Mutation

--- server.js
+++ server.js
@@ -1,61 +1,70 @@
-var express = require('express')
-var { createHandler } = require('graphql-http/lib/use/express')
-var { buildSchema } = require('graphql')
-var { ruruHTML } = require('ruru/server')
+const express = require('express')
+const { createHandler } = require('graphql-http/lib/use/express')
+const { buildSchema } = require('graphql')
+
+const fakeDatabase = {}
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(/* GraphQL */ `
-	type RandomDie {
-		numSides: Int!
-		rollOnce: Int!
-		roll(numRolls: Int!): [Int]
-	}
-
-	type Query {
-		getDie(numSides: Int): RandomDie
-	}
+const schema = buildSchema(`
+input MessageInput {
+  content: String
+  author: String
+}
+ 
+type Message {
+  id: ID!
+  content: String
+  author: String
+}
+ 
+type Query {
+  getMessage(id: ID!): Message
+	getMessages: [Message]
+}
+ 
+type Mutation {
+  createMessage(input: MessageInput): Message
+  updateMessage(id: ID!, input: MessageInput): Message
+}
 `)
 
-// This class implements the RandomDie GraphQL type
-class RandomDie {
-	constructor(numSides) {
-		this.numSides = numSides
-	}
-
-	rollOnce() {
-		return 1 + Math.floor(Math.random() * this.numSides)
-	}
-
-	roll({ numRolls }) {
-		var output = []
-		for (var i = 0; i < numRolls; i++) {
-			output.push(this.rollOnce())
-		}
-		return output
-	}
-}
-
-// The root provides the top-level API endpoints
-var root = {
-	getDie({ numSides }) {
-		return new RandomDie(numSides || 6)
+const root = {
+	getMessage: ({ id }) => {
+		return fakeDatabase[id]
+	},
+	getMessages: () => {
+		return Object.values(fakeDatabase)
+	},
+	createMessage: ({ input }) => {
+		const id = String(Object.keys(fakeDatabase).length + 1)
+		const message = new Message(id, input)
+		fakeDatabase[id] = message
+		return message
+	},
+	updateMessage: ({ id, input }) => {
+		const message = fakeDatabase[id]
+		Object.assign(message, input)
+		return message
 	}
 }
 
-var app = express()
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+	constructor(id, { content, author }) {
+		this.id = id
+		this.content = content
+		this.author = author
+	}
+}
 
-// Serve the GraphiQL IDE.
-app.get('/', (_req, res) => {
-	res.type('html')
-	res.end(ruruHTML({ endpoint: '/graphql' }))
-})
-
+const app = express()
 app.all(
 	'/graphql',
 	createHandler({
 		schema: schema,
-		rootValue: root
+		rootValue: root,
 	})
 )
-app.listen(4000)
-console.log('Running a GraphQL API server at localhost:4000/graphql')
+app.listen(4000, () => {
+	console.log('Running a GraphQL API server at localhost:4000/graphql')
+})
